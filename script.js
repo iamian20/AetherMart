93569578e5fa001c5eb5ee940611fc51c576fec3
Adrian Benedicto <adrxyz20@gmail.com>
2024-06-18 17:25:17 +0800

    fixed the navbar categories on mobile

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const offcanvasNavbar = document.getElementById("offcanvasNavbar");
-  const navLinks = offcanvasNavbar.querySelectorAll(".nav-link");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const offcanvasNavbar = document.getElementById("offcanvasNavbar");
+//   const navLinks = offcanvasNavbar.querySelectorAll(".nav-link");
 
-  navLinks.forEach((link) => {
-    link.addEventListener("click", function () {
-      const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvasNavbar);
-      bsOffcanvas.hide();
-    });
-  });
-});
+//   navLinks.forEach((link) => {
+//     link.addEventListener("click", function () {
+//       const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvasNavbar);
+//       bsOffcanvas.hide();
+//     });
+//   });
+// });
 
 document.addEventListener("DOMContentLoaded", function () {
   const loginButton = document.getElementById("log");
